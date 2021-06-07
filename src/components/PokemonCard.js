5ea103b99e458aace83ec5f8fde8f686f0b199bd
Romel <rantunish20@gmail.com>
2021-06-07 08:41:09 -0400

    Done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard({ pokemon }) {
+  const [showFront, setShowFront] = useState(true);
+  const {name,hp,sprites} = pokemon;
+
+  function handleClick() {
+    setShowFront((showFront) => !showFront);
+  }
   return (
     <Card>
-      <div>
+      <div onClick={handleClick} >
         <div className="image">
-          <img alt="oh no!" />
+          <img src={showFront ? sprites.front : sprites.back}  alt={name} />
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            POKEMON HP {hp}
           </span>
         </div>
       </div>
